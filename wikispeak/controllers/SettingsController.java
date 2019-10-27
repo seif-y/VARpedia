96c118d6af8d663e470a0e30d649c2ef958c223e
hdr00 <hdry406@aucklanduni.ac.nz>
2019-10-27 13:23:30 +1300

    Set up settings page
    Access from settings button home page. Will change this to just a gear
    icon (design standard) to be less intrusive. Now need to implement
    selection functionality.

--- wikispeak/controllers/SettingsController.java
+++ wikispeak/controllers/SettingsController.java
@@ -2,20 +2,36 @@ package wikispeak.controllers;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.IdentityHashMap;
 import java.util.List;
+import java.util.Map;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.text.Text;
-import wikispeak.Creator;
-import wikispeak.creation.Creation;
-import wikispeak.creation.RatingComparator;
-import wikispeak.creation.ViewsComparator;
 
 public class SettingsController extends Controller {
 
     @FXML
     private AnchorPane pane;
+    
+    @FXML
+    private ComboBox<String> themeOptions;
+    private Map<String, String> themeMap = new IdentityHashMap<>();
+    
+    private void initialize() {
+    	ObservableList<String> themes = FXCollections.observableArrayList();
+        
+        themes.addAll("Default", "Yellow");
+        themeOptions.setItems(themes);
+        
+        themeMap.put("Default", "default.css");
+        themeMap.put("Yellow", "yellow.css");
+    }
+    
+    
 
     
     /**
